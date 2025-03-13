24c589fc920f4990b9adfce86d3df7611f82427d
Yassine Hachguer <yassine.hachguer05@gmail.com>
2025-03-13 20:54:24 +0000

    fonction principale main

--- Hospital/src/main/java/com/example/patient/PatientApplication.java
+++ Hospital/src/main/java/com/example/patient/PatientApplication.java
@@ -1,9 +1,18 @@
 package com.example.patient;
 
+import com.example.patient.entities.*;
+import com.example.patient.repository.MedecinRepository;
 import com.example.patient.repository.PatientRepository;
+import com.example.patient.repository.RendezVousRepository;
+import com.example.patient.service.IHospitalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class PatientApplication {
@@ -12,6 +21,50 @@ public class PatientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PatientApplication.class, args);
+    }
+    @Bean
+        //s'exécute au démarrage
+    CommandLineRunner start(IHospitalService hospitalService, MedecinRepository medecinRepository, RendezVousRepository rendezVousRepository){
+        return args -> {
+            Stream.of("Mohammed", "Hassan", "Najat").forEach(name->{
+                Patient patient = new Patient();
+                patient.setNom(name);
+                patient.setMalade(false);
+                patient.setDateNaissance(new Date());
+                hospitalService.savePatient(patient);
+            });
+
+            Stream.of("Aymane", "Hanane", "Yassmine").forEach(name->{
+                Medecin medecin = new Medecin();
+                medecin.setNom(name);
+                medecin.setEmail(name+"@gmail.com");
+                medecin.setSpecialite(Math.random()>0.5?"Cardio":"Dentiste");
+                hospitalService.saveMedecin(medecin);
+            });
+
+            Patient patient = patientRepository.findByNom("Mohammed");
+
+            Medecin medecin = medecinRepository.findByNom("Yassmine");
+
+            RendezVous rendezVous = new RendezVous();
+            rendezVous.setDate(new Date());
+            rendezVous.setStatus(StatusRDV.PENDING);
+            rendezVous.setMedecin(medecin);
+            rendezVous.setPatient(patient);
+
+            hospitalService.saveRDV(rendezVous);
+
+            RendezVous rendezVous1 = rendezVousRepository.findAll().get(0);
+
+            Consultation consultation = new Consultation();
+            consultation.setDateConsultation(new Date());
+            consultation.setRendezVous(rendezVous1);
+            consultation.setRapport("Rapport 1");
+
+
+            hospitalService.saveConsultation(consultation);
+
+        };
     }
 
 
--- Hospital/src/main/java/com/example/patient/repository/RendezVousRepository.java
+++ Hospital/src/main/java/com/example/patient/repository/RendezVousRepository.java
@@ -3,5 +3,5 @@ package com.example.patient.repository;
 import com.example.patient.entities.RendezVous;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RendezVousRepository extends JpaRepository<RendezVous, Long> {
+public interface RendezVousRepository extends JpaRepository<RendezVous, String> {
 }
